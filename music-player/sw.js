1e48dd2b74b2c048b260172e506b71952f55fee0
rafa2468-hub <rafciub@gmail.com>
2026-09-19 07:22:30 -0600

    Add files via upload

--- music-player/sw.js
+++ music-player/sw.js
@@ -1,5 +1,5 @@
 // Bump this string to force clients to fetch the new app shell.
-const CACHE_VERSION = 'music-player-v67';
+const CACHE_VERSION = 'music-player-v68';
 
 const APP_SHELL = [
   './',
